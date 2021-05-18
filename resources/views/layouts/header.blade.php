a1565eb7fff74d223f9b4ebe1b6f8d7adabfff6d
PilotJinix <agungsantoso557@gmail.com>
2021-05-18 17:11:15 +0700

    sinkronisasi data assets

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Ventic : Ticketing Admin Template" />
+<meta property="og:title" content="Ventic : Ticketing Admin Template" />
+<meta property="og:description" content="Ventic : Ticketing Admin Template" />
+<meta property="og:image" content=""/>
+<meta name="format-detection" content="telephone=no">
+
+<!-- PAGE TITLE HERE -->
+<title>Ventic : Ticketing Admin Template</title>
+
+<!-- FAVICONS ICON -->
+<link rel="shortcut icon" type="image/png" href="{{asset("assets/images/favicon.png")}}" />
+
+<link rel="stylesheet" href="{{asset("assets/vendor/chartist/css/chartist.min.css")}}">
+<link href="{{asset("assets/vendor/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css")}}" rel="stylesheet">
+<link href="{{asset("assets/vendor/jquery-nice-select/css/nice-select.css")}}" rel="stylesheet">
+<!-- Style css -->
+<link href="{{asset("assets/css/style.css")}}" rel="stylesheet">
